b6b418e71fe469a1b6c2152d8056addbd69d6c4b
ysj0816 <1334065542@qq.com>
2017-12-13 16:56:21 +0800

    Initial commit

---
+++ svprogresshud/src/main/java/com/yangshijun/svprogresshud/listener/OnDismissListener.java
@@ -0,0 +1,10 @@
+package com.yangshijun.svprogresshud.listener;
+
+import com.yangshijun.svprogresshud.SVProgressHUD;
+
+/**
+ * Created by Sai on 16/7/31.
+ */
+public interface OnDismissListener {
+    void onDismiss(SVProgressHUD hud);
+}
